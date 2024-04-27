e94bf0f3b9b05b6fb4b824da53d849537109636f
My Name <adildotes@gmail.com>
2024-04-27 16:17:33 +0500

    Added JavaScript

--- app.js
+++ app.js
@@ -1,47 +1,56 @@
-
-
-let taskList = []
-// let myData = localStorage.getItem('mytask');
-
-// if (myData !== '') {
-//     taskList = [...taskList, myData]
-// }
-
+let taskList = [];
 
 function addData() {
     let taskInp = document.querySelector("input");
-    let task = taskInp.value;
+    let task = taskInp.value.trim();
     if (task !== "") {
-        update(task)
+        update(task);
     }
     taskInp.value = '';
 }
 
-
 function update(task) {
     if (task !== '') {
-        taskList.push(task)
-        localStorage.setItem('mytask', taskList);
+        taskList.push(task);
+        localStorage.setItem('mytask', JSON.stringify(taskList));
     }
-    let list = document.querySelector("ul");
-    list.innerHTML = '';
-    for (i in taskList) {
-        list.innerHTML += `<li class='bg-li-list d-flex justify-content-between align-items-center'>
-            ${taskList[i]}
-            <div>
-                <i class='fa-solid fa-pen edit-icon'></i>
-                <i onclick="removeTask('${taskList[i]}')" class='fa-regular fa-trash-can ps-3 delete-icon'></i>
-            </div>
-        </li>`;
-    }
+    renderTasks();
 }
 
 function removeTask(content) {
-    let itemToRem = taskList.indexOf(content)
-    taskList.splice(itemToRem, 1)
-    update('')
+    let itemToRem = taskList.indexOf(content);
+    taskList.splice(itemToRem, 1);
+    localStorage.setItem('mytask', JSON.stringify(taskList));
+    renderTasks();
 }
 
+function renderTasks() {
+    let list = document.querySelector("ul");
+    list.innerHTML = '';
+    taskList.forEach((task, index) => {
+        list.innerHTML += `<li class='bg-li-list d-flex justify-content-between align-items-center'>
+            <span class="task-text">${task}</span>
+            <div>
+                <i class='fa-solid fa-pen edit-icon' onclick="editTask(${index})"></i>
+                <i onclick="removeTask('${task}')" class='fa-regular fa-trash-can ps-3 delete-icon'></i>
+            </div>
+        </li>`;
+    });
+}
 
+function editTask(index) {
+    let editedTask = prompt("Edit the task:", taskList[index]);
+    if (editedTask !== null && editedTask !== "") {
+        taskList[index] = editedTask;
+        localStorage.setItem('mytask', JSON.stringify(taskList));
+        renderTasks();
+    }
+}
 
-
+window.onload = function () {
+    let storedTasks = localStorage.getItem('mytask');
+    if (storedTasks) {
+        taskList = JSON.parse(storedTasks);
+        renderTasks();
+    }
+};
